5be6b34cede466103516c62836e6a39e8427f081
Liam De Lee <10959793+Liamdelee@users.noreply.github.com>
2023-01-14 14:27:06 +0100

    adding upload code

---
+++ images/upload/upload_datasets.py
@@ -0,0 +1,23 @@
+import os
+
+import pandas as pd
+from sqlalchemy import create_engine
+
+DATA_FILES = os.environ.get("DATA_FILES").split(',')
+
+user = os.environ.get("MYSQL_USER")
+password = os.environ.get("MYSQL_PASSWORD")
+db_name = os.environ.get("MYSQL_DATABASE")
+
+con_str = "mysql://codetest:swordfish@database/codetest"
+
+# Connect to the database
+engine = create_engine(con_str)
+
+for data_files in DATA_FILES:
+
+    # Read the CSV file into a DataFrame
+    df = pd.read_csv(f'/data0/{data_files}.csv')
+
+    # Write the DataFrame to the database
+    df.to_sql(data_files, engine, if_exists='append', index=False)
