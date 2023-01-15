376b68171849c056c525294d47f164ec8c986d22
Liam De Lee <10959793+Liamdelee@users.noreply.github.com>
2023-01-15 14:19:07 +0100

    add logging.py + add logging in upload and query

--- images/query/query_datasets.py
+++ images/query/query_datasets.py
@@ -1,9 +1,10 @@
 import os
-import logging
-import json
+from common.logger import setup_logger
 
 import pandas as pd
 from sqlalchemy import create_engine
+
+logger = setup_logger("./logs/query_database.log", "DEBUG", "query_database_logger")
 
 user = os.environ.get("MYSQL_USER")
 password = os.environ.get("MYSQL_PASSWORD")
@@ -11,16 +12,23 @@ db_name = os.environ.get("MYSQL_DATABASE")
 
 con_str = f"mysql://{user}:{password}@database/{db_name}"
 
-# Connect to the database
-engine = create_engine(con_str)
+try:
+    # Connect to the database
+    engine = create_engine(con_str)
+except Exception as e:
+    logger.error(f"Failed to create engine: {e}")
 
 query = "SELECT country, COUNT(*) as population FROM people JOIN places ON people.place_of_birth = places.city GROUP BY country;"
 
+logger.info(f"query used: {query}")
 output = pd.read_sql(query,engine)
 
 # output data to json file
-# output.to_json('/data/summary_output.json',orient='values')
-output.set_index("country").to_json('/data/summary_output.json',orient='index')
+json_file = '/data/summary_output.json'
+logger.info(f"writing data to json file: {json_file}")
+output.set_index("country").to_json(json_file,orient='index')
 
 #output data to csv file
+csv_file = '/data/summary_output.csv'
+logger.info(f"writing data to csv file: {csv_file}")
 output.to_csv('/data/summary_output.csv', sep=',', index=False)
